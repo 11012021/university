116fe7a99ba3c6f387f7fa48c0a8fe23be8de783
Mirsaid <narumbaevm@gmail.com>
2021-01-11 22:53:24 +0600

    css admin fix

--- js/reg_list.js
+++ js/reg_list.js
@@ -1,12 +1,10 @@
 let teachersBlock = document.querySelector('#teachersBlock')
 
 function cardTemplate(imgUrl, teacherLink) {
-return card = `
+    return card = `
 <div class="col-md-5 col-sm-12 p-5">
     <div>
-        <img class="w-100"
-            src="${imgUrl}"
-            alt="...">
+        <img class="w-100" src="${imgUrl}" alt="...">
         <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
         <a href="${teacherLink}"><button class="btn btn-primary w-100">Подробнее</button></a>
     </div>
@@ -14,7 +12,7 @@ return card = `
 `
 }
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 10; i++) {
     cardTemplate('https://cutt.ly/OjcRP6y', '/html/profile.html')
     teachersBlock.insertAdjacentHTML('afterbegin', card)
 }
