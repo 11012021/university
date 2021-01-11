3a2052f1919c3165973975f75dc72313eed17060
Mirsaid <narumbaevm@gmail.com>
2021-01-11 20:19:24 +0600

    first commit

---
+++ js/singnin.js
@@ -0,0 +1,10 @@
+// const button_send = document.querySelector('.button_send');
+// const input_e = document.querySelector(".input");
+// const input_p = document.querySelector('.pass');
+
+
+// button_send.addEventListener('click', ()=>{
+//     if(input_e.value == true){
+//         console.log(input_e);
+//     }
+// })
