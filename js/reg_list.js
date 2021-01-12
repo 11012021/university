e6ff9d2f734054e24b194fa301f921a5c84dc735
Mirsaid <narumbaevm@gmail.com>
2021-01-13 03:49:06 +0600

    excel + reglist design

--- js/reg_list.js
+++ js/reg_list.js
@@ -17,17 +17,19 @@ let teachersBlock = document.querySelector('#teachersBlock')
 
 function cardTemplate(imgUrl, teacherLink) {
     return card = `
-<div class="col-md-5 col-sm-12 p-lg-5 p-3">
-    <div>
-        <img class="w-100" src="${imgUrl}" alt="...">
-        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-        <a href="${teacherLink}"><button class="btn btn-primary w-100">Подробнее</button></a>
+<div class="col-lg-4 col-md-5 col-sm-12 p-lg-5 p-3">
+    <div class="card">
+        <div class="card-body p-0">
+            <img class="w-100" src="${imgUrl}" alt="...">
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+            <a href="${teacherLink}"><button class="btn btn-secondary w-100">Подробнее</button></a>
+        </div>
     </div>
 </div>
 `
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 8; i++) {
     cardTemplate('https://cutt.ly/OjcRP6y', '/html/profile.html')
     teachersBlock.insertAdjacentHTML('afterbegin', card)
 }
