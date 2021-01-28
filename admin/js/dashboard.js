7b90d30481dfd4abb0512a2979b94bb1839a8f8b
elmurat24 <elmurat.ta@gmail.com>
2021-01-28 17:08:57 +0600

    admin panel

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -29,18 +29,20 @@ scheduleBtn.addEventListener('click', () => {
 })
 
 // Teachers page
+let teachersBlockRow = document.querySelector('#teachersBlockRow');
+
 function cardTemplate(firstname, lastname, work, imgURL) {
   return card = `
-  <div class="card mt-4" id="${Date.now()}">
-    <div class="card-body">
+  <div class="col-lg-12 pl-lg-5 pr-lg-5 p-3">
+    <div class="card-body bg-white rounded">
       <div class="d-flex flex-column align-items-center text-center">
         <img src="${imgURL}"
           alt="..." class="rounded-circle" width="150">
         <div class="mt-3">
           <h4>${firstname} ${lastname}</h4>
-          <p class="text-secondary mb-3">${work}</p>
+          <p class="text-secondary mb-0">${work}</p>
           <button class="btn btn-danger" id="DELETEteacher">Удалить</button>
-          <button class="btn btn-primary m-2">Отзывы</button>
+          <button class="btn btn-primary">Подробнее</button>
           <button class="btn btn-success">Редактировать</button>
         </div>
       </div>
@@ -49,131 +51,18 @@ function cardTemplate(firstname, lastname, work, imgURL) {
 `
 }
 
-
-
-let teacherBlock = document.querySelector('.teachers');
-
-function cardTemplate(firstname, lastname, age, typeofteacher, room, imgUrl, teacherLink) {
-    return card = `
-  <div class="col-lg-4 col-md-5 col-sm-12 p-lg-5 p-3">
-      <div class="card">
-          <div class="card-body p-0">
-              <img class="w-100" src="${imgUrl}" alt="...">
-              <p><b>${firstname} ${lastname} ${age} лет</b></p>
-              <p><b>Должность:</b> ${typeofteacher}</p>
-              <p><b>Кабинет:</b> ${room}</p>
-              <a href="${teacherLink}"><button class="btn btn-secondary w-100">Подробнее</button></a>
-          </div>
-      </div>
-  </div>
-`
-}
-
 //GET
 fetch('https://parseapi.back4app.com/classes/teachers',
-    {
-        headers: {
-            'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
-            'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
-        }
+  {
+    headers: {
+      'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
+      'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
+    }
+  })
+  .then(response => response.json())
+  .then(json => {
+    json.results.forEach(item => {
+      cardTemplate(item.firstname, item.lastname, item.typeofteacher, item.photo.url)
+      teachersBlockRow.insertAdjacentHTML('beforeend', card)
     })
-    .then(response => response.json())
-    .then(json => {
-        let teachersArray = []
-        json.results.forEach(item => {
-            cardTemplate(item.firstname, item.lastname, item.age, item.typeofteacher, item.room, item.photo.url, '/html/profile.html')
-            teachersArray.unshift(card)
-        })
-        for (let i = 0; i < teachersArray.length; i++) {
-          teacherBlock.insertAdjacentHTML('beforeend', teachersArray[i])
-        }
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// on load draw teachers in main page
-// for (let i = 0; i <= 10; i++) {
-//   cardTemplate(i + 10, i + 100, 'Teacher', 'https://cutt.ly/sjcRGGs');
-//   teachersBlock.insertAdjacentHTML('beforeend', card)
-//   teachersBlock.lastElementChild.classList.add('mb-5')
-// }
-
-// CRUD Teachers
-// let DELETEteacher = document.querySelectorAll('#DELETEteacher')
-// let CREATEteacher = document.querySelector('#CREATEteacher')
-
-// CREATE Teacher
-// CREATEteacher.addEventListener('click', () => {
-//   cardTemplate(prompt('Имя', 'qwerty'), prompt('Фамилия', '123'),
-//     'Teacher', prompt('Фото', 'https://cutt.ly/sjcRGGs'));
-//   teachersBlock.insertAdjacentHTML('beforeend', card)
-//   teachersBlock.lastElementChild.classList.add('mb-5')
-// })
-
-// DELETE Teacher
-// DELETEteacher.forEach(item => {
-//   item.addEventListener('click', (e) => {
-//     let teacherName = e.target.previousElementSibling.previousElementSibling.textContent;
-//     let teachersBlockItem = e.target.parentElement.parentElement.parentElement.parentElement;
-
-//     let ask = confirm('Вы действительно хотите удалить учителя?')
-//     if (ask) {
-//       teachersBlockItem.remove()
-//       teachersBlock.lastElementChild.classList.add('mb-5')
-//       alert(`Учитель ${teacherName} удалён!`)
-//     }
-//   })
-// })
-
-
-
-
-
-// PUT (update teachers data)
-// fetch('https://parseapi.back4app.com/classes/teachers/C8doEph3UN', {
-//   method: 'PUT',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     "X-Parse-Application-Id": 'rSH7h3XKubJC41kGP5HpR9ZZxk6rIBn4wH4OPxvn',
-//     'X-Parse-REST-API-Key': ' w7OBKLeup2Cv965yIpsYJUzuIZw2dUPnc9yJWUbL'
-//   },
-//   body: JSON.stringify({
-//     "Age": 2021
-//   })
-// })
-
-// GET (show teachers data)
-// fetch('https://parseapi.back4app.com/classes/teachers',
-//   {
-//     headers: {
-//       'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
-//       'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(json => {console.log(json.results);})
+  })
--- js/reg_list.js
+++ js/reg_list.js
@@ -1,53 +1,3 @@
-// Navbar-toggler animation
-let navToggler = document.querySelector('.navbar-toggler');
-navToggler.addEventListener('click', () => {
-    if (navToggler.hasAttribute('style')) {
-        navToggler.removeAttribute('style')
-    } else {
-        navToggler.setAttribute('style', 'transform: rotate(90deg) !important;')
-    }
-})
-
-// creating teachersBlock
-let teachersBlock = document.querySelector('#teachersBlock')
-
-function cardTemplate(firstname, lastname, age, typeofteacher, room, imgUrl, teacherLink) {
-    return card = `
-<div class="col-lg-4 col-md-5 col-sm-12 p-lg-5 p-3">
-    <div class="card">
-        <div class="card-body p-0">
-            <img class="w-100" src="${imgUrl}" alt="...">
-            <p><b>${firstname} ${lastname} ${age} лет</b></p>
-            <p><b>Должность:</b> ${typeofteacher}</p>
-            <p><b>Кабинет:</b> ${room}</p>
-            <a href="${teacherLink}"><button class="btn btn-secondary w-100">Подробнее</button></a>
-        </div>
-    </div>
-</div>
-`
-}
-
-// GET (show teachers data)
-fetch('https://parseapi.back4app.com/classes/teachers',
-    {
-        headers: {
-            'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
-            'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
-        }
-    })
-    .then(response => response.json())
-    .then(json => {
-        let teachersArray = []
-        json.results.forEach(item => {
-            cardTemplate(item.firstname, item.lastname, item.age, item.typeofteacher, item.room, item.photo.url, '/html/profile.html')
-            teachersArray.unshift(card)
-        })
-        for (let i = 0; i < teachersArray.length; i++) {
-            teachersBlock.insertAdjacentHTML('afterbegin', teachersArray[i])
-        }
-    })
-
-
 // LOADER
 let page = document.querySelectorAll('.page');
 let loader = document.querySelector('#loader');
@@ -66,3 +16,88 @@ window.addEventListener('load', () => {
     // delete scroll to block after reload
     window.location.hash = 'schedule';
 })
+
+// Navbar-toggler animation
+let navToggler = document.querySelector('.navbar-toggler');
+navToggler.addEventListener('click', () => {
+    if (navToggler.hasAttribute('style')) {
+        navToggler.removeAttribute('style')
+    } else {
+        navToggler.setAttribute('style', 'transform: rotate(90deg) !important;')
+    }
+})
+
+// creating teachersBlock
+let teachersBlock = document.querySelector('#teachersBlock')
+let newsBlock = document.querySelector('#newsBlock')
+
+function cardTemplateTeachers(firstname, lastname, patronymic, age, typeofteacher, room, imgUrl) {
+    return card = `
+<div class="col-lg-4 col-md-5 col-sm-12 p-lg-5 p-3">
+    <div class="card">
+        <div class="card-body p-0">
+            <img class="w-100" src="${imgUrl}" alt="...">
+            <p id="fullname"><b>${firstname} ${lastname} ${patronymic} ${age} лет</b></p>
+            <p><b>Должность:</b> ${typeofteacher}</p>
+            <p><b>Кабинет:</b> ${room}</p>
+            <a href="/html/profile.html" target="_blank"><button class="btn btn-secondary w-100">Подробнее</button></a>
+        </div>
+    </div>
+</div>
+`
+}
+
+function cardTemplateNews(title, body, date) {
+    return card = `
+    <div class="col-md-6 p-3">
+        <div class="p-3" style="background-color: rgba(255, 255, 255, 0.75); border-radius: 10px;">
+            <h4>${title}</h4>
+            <p>${body}</p>
+            <p class="date"><b>Date:</b> ${date}</p>
+        </div>
+    </div>
+`
+}
+
+// GET (teachers)
+fetch('https://parseapi.back4app.com/classes/teachers',
+    {
+        headers: {
+            'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
+            'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        let teachersArray = []
+        json.results.forEach(item => {
+            console.log(item);
+            cardTemplateTeachers(item.firstname, item.lastname, item.patronymic, item.age, item.typeofteacher, item.room, item.photo.url)
+            teachersArray.unshift(card)
+        })
+        for (let i = 0; i < teachersArray.length; i++) {
+            teachersBlock.insertAdjacentHTML('afterbegin', teachersArray[i])
+        }
+    })
+
+// GET (news)
+fetch('https://parseapi.back4app.com/classes/news',
+    {
+        headers: {
+            'X-Parse-Application-Id': 'Sbmh1dqC6y29EhgLdXZsC0UfuG5Ij5LZqTXgmgWz',
+            'X-Parse-REST-API-Key': 'igRmoVIOsA7YH0O31rB3nSkrE7dxMLu0VVHa6rVy'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        let newsArray = []
+        json.results.forEach(item => {
+            let date = new Date(item.updatedAt).toLocaleDateString()
+            cardTemplateNews(item.title, item.body, date)
+            newsArray.unshift(card)
+        })
+        for (let i = 0; i < newsArray.length; i++) {
+            newsBlock.insertAdjacentHTML('afterbegin', newsArray[i])
+        }
+    })
+
